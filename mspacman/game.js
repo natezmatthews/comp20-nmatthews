8e5ab2499681ae1859d1588443c25512e0667e8d
Nate Matthews <natezmatthews@gmail.com>
2015-02-28 16:23:23 -0500

    Got drawImage() working

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,9 @@
-// Replace this with your code...
+function init() {
+	var ctx = document.getElementById('game_canvas').getContext('2d');
+	var img = new Image();   // Create new img element
+	img.addEventListener("load", function() {
+		ctx.drawImage(img,322,2,472,136,0,0,472,136);
+		ctx.drawImage(img,83,23,13,14,37,31,13,14);
+	}, false);
+	img.src = 'pacman10-hp-sprite.png'; // Set source path
+}
